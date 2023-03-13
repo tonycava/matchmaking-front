c8d281b816face55725eca6a2b04f9c77a1aee20
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2023-03-13 12:05:40 +0100

    add: tailwindcss to the frontend

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		host: true, // needed for the DC port mapping to work
+		strictPort: true,
+		port: 3000,
+	}
 });
