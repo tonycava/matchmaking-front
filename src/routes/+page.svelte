9150739836d45068e8f9303b9b11124bfe05c9f4
lucas <lucas.escaffre@ynov.com>
2023-03-22 15:47:38 +0100

    continue: home page when connected

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 	import webSocketClient from '$lib/webSocketClient';
 	import { user } from '@stores/user.store';
 	import { page } from '$app/stores';
+	import PrimaryButton from '@components/button/PrimaryButton.svelte';
+	import { goto } from '$app/navigation';
 
 	let message = '';
 	const sendMessage = async () => {
@@ -19,15 +21,33 @@
 
 </script>
 
-<button>Go to the matchmaking</button>
-
-{#each messages as message}
-	<div class='text-secondary'>
-		{message.content}
+<div class='flex flex-col h-screen'>
+	<div class='text-5xl'>
+		{#if $user}
+			<div class='text-secondary'>
+				{`Hello ${$user.username}`}
+			</div>
+		{/if}
 	</div>
-{/each}
 
-<form on:submit|preventDefault={sendMessage}>
-	<input type="text" bind:value={message}/>
-	<button type="submit">Send message</button>
-</form>
+	<div class='flex flex-1 justify-center items-center [&>button]:text-3xl'>
+		{#if $user}
+			<PrimaryButton on:click={() => goto('/waiting')}>Join the waiting room</PrimaryButton>
+		{:else}
+			<PrimaryButton on:click={goto('/login')}>Login</PrimaryButton>
+		{/if}
+	</div>
+
+	<div class='fixed bottom-0 right-0 border'>
+		{#each messages as message}
+			<div class='text-secondary'>
+				{message.content}
+			</div>
+		{/each}
+
+		<form on:submit|preventDefault={sendMessage}>
+			<input type="text" bind:value={message}/>
+			<button type="submit" class='text-secondary'>Send message</button>
+		</form>
+	</div>
+</div>
