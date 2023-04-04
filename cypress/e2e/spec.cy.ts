f23b33a28159b4c81072c9d08c8095f1461640a5
lucas <lucas.escaffre@ynov.com>
2023-04-04 14:43:04 +0200

    test: unit test front with cypress

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,33 +1,33 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:11001/login')
+    cy.visit('http://localhost:11001/login')
   })
 })
 
 describe('contains username', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:11001/login')
+    cy.visit('http://localhost:11001/login')
     cy.get('input[name=username]').type('test')
   })
 })
 
 describe('contains password', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.111001/login')
+    cy.visit('http://localhost:11001/login')
     cy.get('input[name=password]').type('test')
   })
 })
 
 describe('contains login button', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:11001/login')
+    cy.visit('http://localhost:11001/login')
     cy.contains('Login').click()
   })
 })
 
 describe('the new user have cookies', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:11001/login')
+    cy.visit('http://localhost:11001/login')
     cy.contains('Login').click()
     cy.getCookie('jwt_token').should('exist')
   })
@@ -35,7 +35,7 @@ describe('the new user have cookies', () => {
 
 describe('new URL is the expected URL', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:11001/login')
+    cy.visit('http://localhost:11001/login')
     cy.contains('Login').click()
     cy.url().should('include', '/')
   })
