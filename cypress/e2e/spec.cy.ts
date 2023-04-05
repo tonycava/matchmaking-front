1bbf923fe1dbe7894cc2c7cdfae4981c31f990e9
lucas <lucas.escaffre@ynov.com>
2023-04-05 11:00:17 +0200

    test: unit test front with cypress

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,8 @@
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('http://localhost:11001/login')
-//   })
-// })
+describe('template spec', () => {
+	it('passes', () => {
+		cy.visit('http://localhost:11001/login');
+	});
+});
 //
 // describe('contains username', () => {
 //   it('passes', () => {
@@ -34,19 +34,19 @@
 // })
 
 describe('My First Test', () => {
-  it('Gets, types and asserts', () => {
-    cy.visit('https://example.cypress.io')
+	it('Gets, types and asserts', () => {
+		cy.visit('https://example.cypress.io');
 
-    cy.contains('type').click()
+		cy.contains('type').click();
 
-    // Should be on a new URL which
-    // includes '/commands/actions'
-    cy.url().should('include', '/commands/actions')
+		// Should be on a new URL which
+		// includes '/commands/actions'
+		cy.url().should('include', '/commands/actions');
 
-    // Get an input, type into it
-    cy.get('.action-email').type('fake@email.com')
+		// Get an input, type into it
+		cy.get('.action-email').type('fake@email.com');
 
-    //  Verify that the value has been updated
-    cy.get('.action-email').should('have.value', 'fake@email.com')
-  })
-})
+		//  Verify that the value has been updated
+		cy.get('.action-email').should('have.value', 'fake@email.com');
+	});
+});
