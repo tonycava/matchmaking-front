e1b5e197ef952be632fbea679da5be02be36364b
lucas <lucas.escaffre@ynov.com>
2023-04-05 14:41:33 +0200

    test: unit test front with cypress

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,52 +1,36 @@
 describe('template spec', () => {
-	it('passes', () => {
-		cy.visit('http://localhost:11001/login');
-	});
-});
+  it('passes', () => {
+    cy.visit('http://localhost:11001/login')
+  })
+})
+
+describe('contains username', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:11001/login')
+    cy.get('input[name=username]').type('test')
+  })
+})
+
+describe('contains password', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:11001/login')
+    cy.get('input[name=password]').type('test')
+  })
+})
+
+describe('contains login button', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:11001/login')
+    cy.contains('Login').click()
+  })
+})
+
+describe('new URL is the expected URL', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:11001/login')
+    cy.contains('Login').click()
+    cy.url().should('include', '/')
+  })
+})
+
 //
-// describe('contains username', () => {
-//   it('passes', () => {
-//     cy.visit('http://localhost:11001/login')
-//     cy.get('input[name=username]').type('test')
-//   })
-// })
-//
-// describe('contains password', () => {
-//   it('passes', () => {
-//     cy.visit('http://localhost:11001/login')
-//     cy.get('input[name=password]').type('test')
-//   })
-// })
-//
-// describe('contains login button', () => {
-//   it('passes', () => {
-//     cy.visit('http://localhost:11001/login')
-//     cy.contains('Login').click()
-//   })
-// })
-//
-// describe('new URL is the expected URL', () => {
-//   it('passes', () => {
-//     cy.visit('http://localhost:11001/login')
-//     cy.contains('Login').click()
-//     cy.url().should('include', '/')
-//   })
-// })
-
-describe('My First Test', () => {
-	it('Gets, types and asserts', () => {
-		cy.visit('https://example.cypress.io');
-
-		cy.contains('type').click();
-
-		// Should be on a new URL which
-		// includes '/commands/actions'
-		cy.url().should('include', '/commands/actions');
-
-		// Get an input, type into it
-		cy.get('.action-email').type('fake@email.com');
-
-		//  Verify that the value has been updated
-		cy.get('.action-email').should('have.value', 'fake@email.com');
-	});
-});
