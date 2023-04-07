ff43ce21f2d85b39dc46c432f73825e8343377ac
tonycava <anthony.cavagne@ynov.com>
2023-04-07 15:23:09 +0200

    add: login and register responsive

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -8,11 +8,11 @@
 	export let form: FormResponse;
 </script>
 
-<div class="flex justify-center flex flex-col gap-1">
+<div class="flex justify-center flex-col gap-1">
 	{#if form?.internalError}
 		<span class="text-sm text-red-600 text-center font-poppins-bold">{form?.internalError}</span>
 	{/if}
-	<PrimaryButton type="submit">Register</PrimaryButton>
+	<PrimaryButton css="mx-auto" type="submit">Register</PrimaryButton>
 </div>
 
 <span class="absolute text-sm bottom-0 left-0 p-3 text-secondary font-poppins-medium">
