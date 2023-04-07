ffdc85b21731faa18a1c1fec788434e2adf31b8c
tonycava <anthony.cavagne@ynov.com>
2023-04-07 15:47:35 +0200

    add: error handling page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { page } from '$app/stores';
+  import PrimaryButton from '@components/button/PrimaryButton.svelte';
+  import { goto } from '$app/navigation';
+</script>
+
+<div class="flex justify-center items-center flex-col h-screen gap-8">
+  <h1 class="text-secondary font-poppins-medium text-3xl text-center">Are you lost ?</h1>
+  <PrimaryButton on:click={() => goto('/')}>Go back on a safe place</PrimaryButton>
+</div>
+{JSON.stringify($page.error)}
