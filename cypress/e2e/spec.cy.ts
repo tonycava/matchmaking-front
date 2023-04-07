97a8570c070d7ab345a5a8977f7a94cf31273309
tonycava <anthony.cavagne@ynov.com>
2023-04-07 16:53:39 +0200

    add: cypress config

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,30 @@
+describe('template spec', () => {
+	it('passes', () => {
+		cy.visit('http://localhost:11001')
+	})
+})
+
+describe('contains username', () => {
+	it('passes', () => {
+		cy.visit('http://localhost:11001/login')
+		cy.get('input[name=username]').type('test')
+		cy.get('input[name=username]').should('have.value', 'test')
+	})
+})
+
+describe('contains login button', () => {
+	it('passes', () => {
+		cy.visit('http://localhost:11001/login')
+		cy.contains('Login').click()
+	})
+})
+
+describe('new URL is the expected URL', () => {
+	it('passes', () => {
+		cy.visit('http://localhost:11001/login')
+		cy.contains('Login').click()
+		cy.url().should('include', '/')
+	})
+})
+
+//
