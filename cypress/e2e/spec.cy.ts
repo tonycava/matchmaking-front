e4a3ef4552a52387123745248ad902a966ec35da
tonycava <anthony.cavagne@ynov.com>
2023-04-16 22:04:21 +0200

    test: small change

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,29 +1,29 @@
-const PORT = Cypress.env('PORT')
+const BASE_URL = `http://localhost:${Cypress.env('PORT') ?? 11001}`;
 
 describe('template spec', () => {
-	it('passes', () => {
-		cy.visit(`http://localhost:${PORT}`);
+	it('Is open', () => {
+		cy.visit(BASE_URL);
 	});
 });
 
 describe('contains username', () => {
-	it('passes', () => {
-		cy.visit(`http://localhost:${PORT}/login`);
+	it('Can type in username field', () => {
+		cy.visit(BASE_URL);
 		cy.get('input[name=username]').type('test');
 		cy.get('input[name=username]').should('have.value', 'test');
 	});
 });
 
 describe('contains login button', () => {
-	it('passes', () => {
-		cy.visit(`http://localhost:${PORT}/login`);
+	it('can click', () => {
+		cy.visit(BASE_URL);
 		cy.contains('Login').click();
 	});
 });
 
 describe('new URL is the expected URL', () => {
-	it('passes', () => {
-		cy.visit(`http://localhost:${PORT}/login`);
+	it('does redirect ?', () => {
+		cy.visit(BASE_URL);
 		cy.contains('Login').click();
 		cy.url().should('include', '/');
 	});
