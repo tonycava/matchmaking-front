43c8e8186d25764fefb2829350452efdc910d8be
tonycava <anthony.cavagne@ynov.com>
2023-04-16 21:37:50 +0200

    add: dynamic port to cypress test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,12 +1,15 @@
+const PORT: number = +process.env.PORT || 11001;
+
+
 describe('template spec', () => {
 	it('passes', () => {
-		cy.visit('http://localhost:11001');
+		cy.visit(`http://localhost:${PORT}`);
 	});
 });
 
 describe('contains username', () => {
 	it('passes', () => {
-		cy.visit('http://localhost:11001/login');
+		cy.visit(`http://localhost:${PORT}/login`);
 		cy.get('input[name=username]').type('test');
 		cy.get('input[name=username]').should('have.value', 'test');
 	});
@@ -14,14 +17,14 @@ describe('contains username', () => {
 
 describe('contains login button', () => {
 	it('passes', () => {
-		cy.visit('http://localhost:11001/login');
+		cy.visit(`http://localhost:${PORT}/login`);
 		cy.contains('Login').click();
 	});
 });
 
 describe('new URL is the expected URL', () => {
 	it('passes', () => {
-		cy.visit('http://localhost:11001/login');
+		cy.visit(`http://localhost:${PORT}/login`);
 		cy.contains('Login').click();
 		cy.url().should('include', '/');
 	});
