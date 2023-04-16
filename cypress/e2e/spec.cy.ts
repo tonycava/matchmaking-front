f4dc4b9f571cd1bd0ac2f0a85f065f230247c17b
tonycava <anthony.cavagne@ynov.com>
2023-04-16 22:08:50 +0200

    test: small change

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,29 +1,29 @@
-const BASE_URL = `http://localhost:${Cypress.env('PORT') ?? 11001}`;
+const PORT: number = Cypress.env('PORT') || 11001;
 
 describe('template spec', () => {
-	it('Is open', () => {
-		cy.visit(BASE_URL);
+	it('passes', () => {
+		cy.visit(`http://localhost:${PORT}`);
 	});
 });
 
 describe('contains username', () => {
-	it('Can type in username field', () => {
-		cy.visit(BASE_URL);
+	it('passes', () => {
+		cy.visit(`http://localhost:${PORT}/login`);
 		cy.get('input[name=username]').type('test');
 		cy.get('input[name=username]').should('have.value', 'test');
 	});
 });
 
 describe('contains login button', () => {
-	it('can click', () => {
-		cy.visit(BASE_URL);
+	it('passes', () => {
+		cy.visit(`http://localhost:${PORT}/login`);
 		cy.contains('Login').click();
 	});
 });
 
 describe('new URL is the expected URL', () => {
-	it('does redirect ?', () => {
-		cy.visit(BASE_URL);
+	it('passes', () => {
+		cy.visit(`http://localhost:${PORT}/login`);
 		cy.contains('Login').click();
 		cy.url().should('include', '/');
 	});
