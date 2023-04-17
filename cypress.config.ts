1ceb951de6eedd7b4dea53d9a34e290d61cc5feb
lucas <lucas.escaffre@ynov.com>
2023-04-17 14:46:50 +0200

    test: test login, register and start testing main page

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
-        baseUrl: 'http://localhost:11001',
+        baseUrl: 'http://localhost:8089',
 	}
 });
