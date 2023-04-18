6a8ab561cdaaed2ca539b7d645417e311392bd99
tonycava <anthony.cavagne@ynov.com>
2023-04-18 11:07:20 +0200

    change: websocket transport

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -18,7 +18,7 @@ const ENDPOINT = getBaseURL() ?? 'http://localhost:3000';
 
 const webSocketClient = io(ENDPOINT, {
 	path: '/socket.io',
-	transports: ['websocket', 'polling'],
+	transports: ['polling'],
 	secure: true,
 });
 export default webSocketClient;
