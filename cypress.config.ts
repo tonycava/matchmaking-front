309ecd936191e8fb25e3ddd9cd2eb637d007e09f
lucas <lucas.escaffre@ynov.com>
2023-04-19 14:03:02 +0200

    fix: video problem xvfb problem

--- cypress.config.ts
+++ cypress.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
 						)
 						if (!failures) {
 							// delete the video if the spec passed and no tests retried
-							fs.unlinkSync(results.video)
+							fs.unlinkSync(`/home/runner/work/matchmaking/matchmaking/frontend/cypress/videos/${spec.name}.mp4`)
 						}
 					}
 				}
