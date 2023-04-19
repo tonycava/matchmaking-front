680137c357cef19d11950a756e41a2822958e570
lucas <lucas.escaffre@ynov.com>
2023-04-19 14:07:38 +0200

    fix: video problem xvfb problem

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,25 +1,9 @@
 import { defineConfig } from 'cypress';
-import * as fs from "fs";
 export default defineConfig({
 	projectId: '9mzfyx',
 	responseTimeout: 120e3,
 	e2e: {
 		setupNodeEvents(on, config) {
-			on(
-				'after:spec',
-				(spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
-					if (results && results.video) {
-						// Do we have failures for any retry attempts?
-						const failures = results.tests.some((test) =>
-							test.attempts.some((attempt) => attempt.state === 'failed')
-						)
-						if (!failures) {
-							// delete the video if the spec passed and no tests retried
-							fs.unlinkSync(`/home/runner/work/matchmaking/matchmaking/frontend/cypress/videos/${spec.name}.mp4`)
-						}
-					}
-				}
-			)
 			config.env.test =
 				process.env.NODE_ENV === 'test';
 			return config;
